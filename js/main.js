0e5afa6fdc05a6dbc49f3a91f8aa481b9620aa5e
A-S-Maddy <foxxtar.styles@gmail.com>
2020-08-01 17:09:58 +0530

    Complaints handled!!

--- js/main.js
+++ js/main.js
@@ -13,10 +13,17 @@ function hov(nav, e){
 hov(".men", ".men-link");
 hov(".women", ".women-link" );
 hov(".accessories", ".access-link");
-hov(".wallet-list-wrap", ".wallet-pop");
-hov(".category" , ".category a");    
-            document.addEventListener('click', showItem);   
-    const buyBtn = document.querySelector('.buy-btn');
+hov(".wallet-list-wrap", ".wallet-pop");    
+if(document.body.offsetWidth <= 520){
+    hov(".category" , ".castegory-wrap");    
+    $(".category-wrap").css('display','flex');
+    }
+    
+    let cost,Cost = 0;
+    let item =  document.querySelectorAll('.item-img'); 
+    document.addEventListener('click', showItem);   
+   
+    
     const close = document.querySelector('.close'); 
     const shadow = document.querySelector('.covering-block');
     const display = document.querySelector('.card');
@@ -26,67 +33,48 @@ hov(".category" , ".category a");
 	let name = document.querySelector('.card-name');
 	let wallet = document.querySelector('.amount');
 	let balance = parseInt(wallet.innerText.slice(1),10);
-    
+     const buyBtn = document.querySelector('#buy');
+  
 function showItem(e){
 	let element = e;
-	console.log(e)
+   
         if (element.srcElement.className == 'item-img slide-img'){
             shadow.style.display = 'grid';
             img.setAttribute('src',e.srcElement.currentSrc);   
             name.innerText = e.srcElement.nextElementSibling.childNodes[1].innerText;
             head.innerText = e.srcElement.parentElement.parentElement.previousElementSibling.innerHTML;
             cost = e.srcElement.parentElement.childNodes[5].childNodes[3].innerHTML;
-           let Cost = parseInt(cost.slice(1),10);
-            
-                price.innerText = cost;
-            function buy() {
-		      balance -= Cost;
-                alert("Added in cart:"+name.innerText);
-		      if (Cost > balance){
-			     alert('Not enough money!!');
-                  return null;
-		      }
-                wallet.innerText = `$${balance}`;
-                alert('Remaining Balance :'+balance)
-		      return balance;
-            }
-        buyBtn.addEventListener('click',buy);
+          price.innerText = cost;
+         Cost = parseInt(cost.slice(1),10);                 
         }
+    else if (element.srcElement.className == 'buy-btn'){
+        if (balance < Cost){
+            alert("Sorry, Not enough money!!")
+        }
+                else{
+                    balance -= Cost;
+                    alert("Added in cart:"+name.innerText);
+                wallet.innerText = `$${balance}`;
+                alert('Remaining Balance :'+balance);
+                }
+    }
+             
     
-    }   
-    
+        }
+//      function buy(f){
+//        f.onclick = function() {
+//		      balance -= Cost;
+//                alert("Added in cart:"+name.innerText);
+//                wallet.innerText = `$${balance}`;
+//                alert('Remaining Balance :'+balance);
+//                return balance;
+//            }    
         function closeCard(e){
             e.onclick = function(){
             shadow.style.display = 'none'; 
         }
     
-    }
-       closeCard(shadow || close);
+        }
+        closeCard(close);
+
 }
-// for (i in Womens) {
-// 	for (j in Womens[i]){
-// 		if(i ==0 )
-// 			{
-			
-// 			console.log(img.src);
-// 		}
-// 	}
-// }
-
-// const display = document.createElement(div);
-// const shadow = document.createElement(div);
-// const item = $('.item-image');
-// const itemName = $('.item-name');
-// const price = $('.item-cost');
-// let icons = {cart:'.fas fa-cart-plus', bookmark:'.fas fa-bookmark', comment:'.fas fa-msg-bubble',review:'.fas fa-eye'};
-// const buyBtn = document.createElement(button);
-// const wishBtn = document.createElement(button);
-
-// display.className('display');
-// shadow.className('shadow');
-// buyBtn.className('buyBtn');
-// wishBtn.className('.wishBtn');
-
-// document.body.appendChild(shadow);
-// document.display.append(item,itemName,price
-
